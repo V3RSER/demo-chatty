7410879a305fbe546c29e80c692a0797e725d90d
V3RSER <luisdeiko@outlook.com>
2022-04-05 02:50:21 -0500

    React-router implementado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import PublicRoute from "./components/PublicRoute";
+import PrivateRoute from "./components/PrivateRoute";
+import { auth } from "./services/firebase";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading: false,
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          loading: false,
+        });
+      }
+    });
+  }
+
+  render() {
+    return this.state.loading === true ? (
+      <h2>Loading...</h2>
+    ) : (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <PrivateRoute
+            path="/chat"
+            authenticated={this.state.authenticated}
+            component={Chat}
+          ></PrivateRoute>
+          <PublicRoute
+            path="/signup"
+            authenticated={this.state.authenticated}
+            component={Signup}
+          ></PublicRoute>
+          <PublicRoute
+            path="/login"
+            authenticated={this.state.authenticated}
+            component={Login}
+          ></PublicRoute>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
